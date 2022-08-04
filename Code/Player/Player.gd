777c43dd3b8cc013fbeb4b00a3ca44e56c1afe9e
unblinky <47279470+unblinky@users.noreply.github.com>
2022-08-03 18:26:19 -0700

    Player basic moves.

--- Code/Player/Player.gd
+++ Code/Player/Player.gd
@@ -1,2 +1,14 @@
 extends Area
 class_name Player
+
+var speed = 2.0
+
+func _process(delta):
+	if Input.is_action_just_pressed("move_forward"):
+		translation.z += -speed
+	if Input.is_action_just_pressed("move_back"):
+		translation.z += speed
+	if Input.is_action_just_pressed("move_left"):
+		translation.x += -speed
+	if Input.is_action_just_pressed("move_right"):
+		translation.x += speed
