c1a3f309db9c54d602b68c3ace9d04fd307e0e47
unblinky <47279470+unblinky@users.noreply.github.com>
2022-08-14 14:54:31 -0700

    Player is alive and stuff.

--- Code/Main/Main.gd
+++ Code/Main/Main.gd
@@ -14,7 +14,20 @@ func _ready():
 
 func SpawnPlayer():
 	var player = PLAYER.instance()
+	player.main = self
 	player.SetPositionUI(players.size())
 	player.SetPosition3D(players.size())
 	players.append(player)
 	add_child(player)
+
+
+# TODO: Untested.
+func IsGameOver():
+	# Are all the players out?
+	for player in players:
+		if player.is_out == false: # TODO: player.lives > 0:
+			return false
+			
+	# Game is over.
+	# TODO: Display UI > Main Menu
+	return true
--- Code/Player/Player.gd
+++ Code/Player/Player.gd
@@ -7,17 +7,20 @@ var death_audio : Array
 var goal_audio : Array
 
 # Logic
+var main : Node
 var speed = 2.0
 var lives = 4 # Decrement on Player Spawn.
 var offset_ui = 200 # Pixels.
 var offset_3d = 3 # Meters.
 var parent_vessel: Vessel = null
+var is_alive = false
 
 
 func _ready():
 	movement_audio.append($move_0)
 	movement_audio.append($move_1)
 	UpdateLives(-1)
+	is_alive = true
 
 
 func _process(delta):
@@ -54,10 +57,17 @@ func SetPosition3D(player_count):
 
 
 func UpdateLives(value):
+	if value < 0:
+		is_alive = false
+		$Chicken.hide()
+		$Tomb.show()
+	
 	lives += value
 	$UI/Label.text = str(lives)
 	if lives <= 0:
-		queue_free()
+		main.IsGameOver()
+		print("Tell Main we are done so it can wait fro others ot fiish.")
+		#queue_free()
 
 
 func PlayRandomSound(array):
@@ -67,24 +77,27 @@ func PlayRandomSound(array):
 
 
 func _on_Player_area_entered(area):
-	if area is Vehicle:
-		print("Hey a car hit me.")
-		UpdateLives(-1)
-	
-	if area is Vessel:
-		parent_vessel = area
-		print("Riding a boat.")
-		# TODO: Vessel list
-		# Reparent to boat.
-		# or update positon to boat.
-	
-	if area is River:
-		if parent_vessel == null:
+	if is_alive:
+		if area is Vehicle:
 			UpdateLives(-1)
-			print("Fell in river.")
-	
-	if area is Goal:
-		print("Landed on a goal.")
+			is_alive = false
+			print("Hey a car hit me.")
+		
+		if area is Vessel:
+			parent_vessel = area
+			print("Riding a boat.")
+			# TODO: Vessel list
+			# Reparent to boat.
+			# or update positon to boat.
+		
+		if area is River:
+			if parent_vessel == null:
+				UpdateLives(-1)
+				is_alive = false
+				print("Fell in river.")
+		
+		if area is Goal:
+			print("Landed on a goal.")
 		# TODO:
 		# Ends somehow.
 		# tell main that one of our gaols is complete.
