0ba753a790dd55d137662f05e28d5f68a73a34bc
unblinky <>
2024-07-08 18:32:10 -0700

    Single move on pressed.

--- Code/Frog/Frog.gd
+++ Code/Frog/Frog.gd
@@ -7,21 +7,32 @@ const JUMP_VELOCITY = 4.5
 var velocity: Vector3 = Vector3.ZERO
 
 
+
+
 func _ready():
 	area_entered.connect(OnAreaEntered)
 
 
 func _physics_process(delta):
-	var input_dir = Input.get_vector("move_left", "move_right", "move_fore", "move_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if Input.is_action_just_pressed("move_left"):
+		position.x -= 1.0
+	if Input.is_action_just_pressed("move_right"):
+		position.x += 1.0
+	if Input.is_action_just_pressed("move_fore"):
+		position.z -= 1.0
+	if Input.is_action_just_pressed("move_back"):
+		position.z += 1.0
 	
-	position += velocity * delta
+	#var input_dir = Input.get_vector("move_left", "move_right", "move_fore", "move_back")
+	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	#if direction:
+		#velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	#position += velocity * delta
 
 
 func OnAreaEntered(area):
