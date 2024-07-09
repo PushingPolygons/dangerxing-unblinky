ac07172deba2bb7834282b4449fc91fff04b5625
unblinky <>
2024-07-08 19:00:18 -0700

    Movement finished.

--- Code/Frog/Frog.gd
+++ Code/Frog/Frog.gd
@@ -1,38 +1,39 @@
 extends Area3D
 class_name Frog
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-var velocity: Vector3 = Vector3.ZERO
-
-
+var current_position: Vector3
+var target_position: Vector3
+var weight: float = 0.0
+var speed: float = 2.0
+var stride_length: float = 1.0
 
 
 func _ready():
 	area_entered.connect(OnAreaEntered)
+	current_position = Vector3(0.0, 0.0, 0.0)
 
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("move_left"):
-		position.x -= 1.0
-	if Input.is_action_just_pressed("move_right"):
-		position.x += 1.0
-	if Input.is_action_just_pressed("move_fore"):
-		position.z -= 1.0
-	if Input.is_action_just_pressed("move_back"):
-		position.z += 1.0
-	
-	#var input_dir = Input.get_vector("move_left", "move_right", "move_fore", "move_back")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * SPEED
-		#velocity.z = direction.z * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-		#velocity.z = move_toward(velocity.z, 0, SPEED)
-	
-	#position += velocity * delta
+	if current_position != target_position:
+		weight += speed * delta
+		if weight >= 1.0:
+			weight = 1.0
+			current_position = target_position
+		
+		position = lerp(current_position, target_position, weight)
+	else:
+		if Input.is_action_just_pressed("move_left"):
+			target_position.x -= stride_length
+			weight = 0.0
+		if Input.is_action_just_pressed("move_right"):
+			target_position.x += stride_length
+			weight = 0.0
+		if Input.is_action_just_pressed("move_fore"):
+			target_position.z -= stride_length
+			weight = 0.0
+		if Input.is_action_just_pressed("move_back"):
+			target_position.z += stride_length
+			weight = 0.0
 
 
 func OnAreaEntered(area):
